3aa70131a902738061669d3a5ad75b3f220fd437
frontforce <hatifnatts@gmail.com>
2018-02-26 17:28:22 +0800

    color change

--- js/canvas.js
+++ js/canvas.js
@@ -2,7 +2,6 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
-
-
-ctx.fillRect(50, 50, 100, 200);
 ctx.fillStyle = '#cccccc';
+ctx.fillRect(50, 50, 100, 200);
+
