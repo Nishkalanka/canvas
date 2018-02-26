289278d6026c21fcb05b0db0bbe44d2e51fc2e60
frontforce <hatifnatts@gmail.com>
2018-02-26 17:31:44 +0800

    ch

--- js/canvas.js
+++ js/canvas.js
@@ -2,6 +2,6 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
-ctx.fillStyle = '#cccccc';
-ctx.fillRect(50, 50, 100, 200);
-
+//ctx.fillStyle = '#cccccc';
+ctx.rect(50, 50, 100, 200);
+ctx.stroke();
