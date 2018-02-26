3491dc42ea2694a914b93b1142012c0469e44e05
frontforce <hatifnatts@gmail.com>
2018-02-26 17:11:36 +0800

    2string add

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 <head>
 <meta charset="UTF-8">
   <title>C A N V A S</title>
-  <link href="style/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+  <link href="style/screen.css" rel="stylesheet" type="text/css" />
   <link href="style/style.css" rel="stylesheet" type="text/css" />
-  <script 
+   
 </head>
 
 <body>
-  
+  <canvas id="canvas" width="150" height="150"></canvas>
 </body>
 
 </html>
--- js/canvas.js
+++ js/canvas.js
@@ -1,4 +1,6 @@
 
-function(){
-  
-};
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+
+ctx.fillRect(50, 50, 100, 200);
