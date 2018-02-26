f252bbd20b717638be7fcf3d957ccb31bd3ed146
frontforce <hatifnatts@gmail.com>
2018-02-26 17:49:28 +0800

    fd

--- js/canvas.js
+++ js/canvas.js
@@ -4,7 +4,7 @@ var ctx = canvas.getContext('2d');
 
 //ctx.fillStyle = '#cccccc';
 ctx.rect(50, 50, 100, 200);
-ctx.moveTo(75, 75);
-ctx.lineTo(25,25);
-ctx.lineTo(25,0);
+//ctx.moveTo(75, 75);
+//ctx.lineTo(25,25);
+//ctx.lineTo(25,0);
 ctx.stroke();
--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var ctx = canvas.getContext('2d');
 
 for(t = 0; t < 100; t+=10)
 {
-  ctx.moveTo(0, 0);
+  ctx.moveTo(t, 0);
   ctx.lineTo(100,t);
 }
 
